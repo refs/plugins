2fce4ee6b7083406a59bce40ab3be8d5156906fc
A.Unger <zyxancf@gmail.com>
2021-07-13 22:12:47 +0200

    duh - redundant

--- loader/main.go
+++ loader/main.go
@@ -10,10 +10,6 @@ import (
 var plugins = []string{"../plugins/englishGreeter/main.so", "../plugins/chineseGreeter/main.so"}
 
 func main() {
-	// 1. load a plugin
-	// 2. run the Serve function
-	// 3. iterate over an array of plugins and call the serve function?
-	// How can one enforce a contract on a plugin? A: Lookup should serve as enforcing a contract
 	halt := make(chan os.Signal, 1)
 	signal.Notify(halt, os.Interrupt)
 
